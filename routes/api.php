1a065df7bb1a076da3c1ee51f27e01b6f1c0121f
renantdai <renankh@hotmail.com>
2024-08-05 19:34:28 -0300

    primeiro commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', function(Request $request){
+    $credentials = $request->only(['email', 'password']);
+
+    if(!$token = auth()->attempt($credentials)){
+        abort(401, 'Não autorizado');
+    }
+
+    return response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]
+    ]);
+});
